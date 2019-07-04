c920c23d574a8d45d0c203e1d2a4a84dde27077f
flmoro <fernandoluizmoro@gmail.com>
2019-07-04 17:15:33 -0300

    Add metadata to the models

--- sellgood/models.py
+++ sellgood/models.py
@@ -6,9 +6,12 @@ class Plan(models.Model):
     minimum_value = models.DecimalField(max_digits=7, decimal_places=2)
     lower_percentage = models.DecimalField(max_digits=3, decimal_places=2)
     higher_percentage = models.DecimalField(max_digits=3, decimal_places=2)
+
+    class Meta:
+        ordering = ['name']
     
     def __str__(self):
-        return f'{self.name}, {self.minimum_value}'
+        return self.name
 
 
 class Seller(models.Model):
@@ -18,6 +21,9 @@ class Seller(models.Model):
     phone = models.CharField(max_length=13)
     email = models.CharField(max_length=100)
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return self.name
@@ -35,6 +41,9 @@ class Address(models.Model):
                                   on_delete=models.CASCADE, 
                                   primary_key=True)
 
+    class Meta:
+        ordering = ['seller']
+
     def __str__(self):
         return f'{self.seller.name}, {self.city}'
 
@@ -46,6 +55,9 @@ class Sale(models.Model):
                                      decimal_places=2, 
                                      editable=False)   
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['date']
 
     def __str__(self):
         return f'{self.date}, {self.value}, {self.seller.name}'
