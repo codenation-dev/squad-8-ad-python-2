a042c232727a21cd4a673c1ab43243e3ce8e2847
flmoro <fernandoluizmoro@gmail.com>
2019-07-04 12:55:50 -0300

    Add models
    First models definitions.
    
    Co-authored-by: messerzen <39966249+messerzen@users.noreply.github.com>
    Co-authored-by: gpontesss <pontes.guisilva@gmail.com>
    Co-authored-by: Rafael-Fonseca <rafael.abreu.unb@gmail.com>
    Co-authored-by: martinsvanessa <18190437+martinsvanessa@users.noreply.github.com>

--- sellgood/models.py
+++ sellgood/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Plan(models.Model):
+    name = models.CharField(max_length=50)
+    minimum_value = models.DecimalField(max_digits=6, decimal_places=2)
+    lower_percentage = models.DecimalField(max_digits=1, decimal_places=4)
+    higher_percentage = models.DecimalField(max_digits=1, decimal_places=4)
+    
+    def __str__(self):
+        return f'{self.name}, {self.minimum_value}'
+
+
+class Seller(models.Model):
+    cpf = models.CharField(max_length=11)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    phone = models.CharField(max_length=13)
+    email = models.CharField(max_length=100)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):    
+    street = models.CharField(max_length=100)
+    neighborhood = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=30)
+    number = models.CharField(max_length=7)
+    complement = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=8)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.seller.name}, {self.city}'
+
+
+class Sale(models.Model):
+    date = models.DateField()
+    value = models.DecimalField(max_digits=6, decimal_places=2)  
+    commission = models.DecimalField(max_digits=5, 
+                                     decimal_places=2, 
+                                     editable=False)   
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.date}, {self.value}, {self.seller.name}'
