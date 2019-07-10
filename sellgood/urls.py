d066069c83340e311879124b46c78a6b6cd23b3b
flmoro <fernandoluizmoro@gmail.com>
2019-07-10 19:51:35 -0300

    Add commission urls to the app

---
+++ sellgood/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, re_path
+
+from .views import commission
+
+
+urlpatterns = [
+    path('commission/rank/', commission.rank, name='rank'),
+    re_path(r'commission/rank/(?P<year>\d{4})$', 
+            commission.rank_year, 
+            name='rank_year'),
+    re_path(r'commission/rank/(?P<month>\d{1,2})$', 
+            commission.rank_month, 
+            name='rank_month'), 
+    path('commission/rank/<int:year>/<int:month>', 
+         commission.rank_year_month, 
+         name='rank_year_month')
+]
