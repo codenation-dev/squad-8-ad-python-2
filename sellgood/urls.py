7447531c864dfb77697bac3ac2497a8124cba784
messerzen <phzm.engmec@gmail.com>
2019-07-11 14:15:41 -0300

    Add list_sales_month url path in urls.py

--- sellgood/urls.py
+++ sellgood/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from views import sale
 
 urlpatterns = [
     path('sale', sale.create_read_sale),
     path('sale/<int:id_sale>', sale.update_delete_sale),
-    path('sale/seller/<int:id_seller>', views.sale_list_seller)
+    path('sale/seller/<int:id_seller>', views.sale_list_seller),
+    re_path(r'sale/rank/(?P<month>\d{2}$)', views.list_sales_month,                       name='rank_sale_month'),
 ]
