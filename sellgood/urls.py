c292fca686bca4916d007bdca8e3f692b00193bf
messerzen <phzm.engmec@gmail.com>
2019-07-11 13:15:33 -0300

    add sale_list_saler view path to urls

--- sellgood/urls.py
+++ sellgood/urls.py
@@ -4,4 +4,5 @@ from views import sale
 urlpatterns = [
     path('sale', sale.create_read_sale),
     path('sale/<int:id_sale>', sale.update_delete_sale),
+    path('sale/seller/<int:id_seller>', views.sale_list_seller)
 ]
