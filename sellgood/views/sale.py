b0ddbb4b95904230b5cf7feb4486fccf6ddc132e
messerzen <phzm.engmec@gmail.com>
2019-07-11 10:03:54 -0300

    update_sale updated to check request method and validate data before update sale

--- sellgood/views/sale.py
+++ sellgood/views/sale.py
@@ -1,12 +1,13 @@
 from django.shortcuts import HttpResponse
-from .models import Sale
+from sellgood.models import Sale
 from sellgood.forms import SaleForm
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
 
+
 @csrf_exempt
-def create_sale(request): # Create Sale
+def create_sale(request):  # Create Sale
     if request.method == 'POST':  # Check request method
         sale_info = json.loads(request.body)
         form = SaleForm(sale_info)
@@ -39,15 +40,29 @@ def create_sale(request): # Create Sale
 
 @csrf_exempt
 def update_sale(request, id_sale):
-    sale_upd_info = json.loads(request.body)
-    sale_to_update = Sale.objects.filter(pk=id_sale)
-    for obj in sale_to_update:
-        obj.date = sale_upd_info['date']
-        obj.amount = sale_upd_info['amount']
-        obj.save()
-    response = {
-        'sale_id': id_sale
-    }
+    if request.method == 'PUT':   # Check request method
+        sale_info = json.loads(request.body)
+        form = SaleForm(sale_info)
+        if form.is_valid():      # Validate data
+            sale_to_update = Sale.objects.get(pk=id_sale)
+            sale_to_update.date = form.cleaned_data['date']
+            sale_to_update.amount = form.cleaned_data['amount']
+            sale_to_update.seller = form.cleaned_data['seller']
+            sale_to_update.save()
+            body_content = {
+                'sale_id': id_sale
+            }
 
-    return HttpResponse(json.dumps(response),content_type='application/json',
-                        status=205)
+             # Return updated_sale id 
+            return JsonResponse(response, status=200)
+            
+        # Since body not valid, return errors    
+        else:
+            return JsonResponse(form.errors, status=422)
+
+    # If method is different from PUT, return body_content        
+    else:
+        body_content = {
+            'error': 'Method not allowed'
+        }
+        return JsonResponse(body_content, status=405)
