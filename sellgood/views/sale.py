1d8f206f9a2a37a75d916d2dc2c0720f620a17e8
messerzen <phzm.engmec@gmail.com>
2019-07-11 09:41:33 -0300

    create_sale updated to validate data using ModelForm before create a sale; and check request method before validade data

--- sellgood/views/sale.py
+++ sellgood/views/sale.py
@@ -1,22 +1,40 @@
 from django.shortcuts import HttpResponse
 from .models import Sale
+from sellgood.forms import SaleForm
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
 
 @csrf_exempt
-def create_sale(request):
-    sale_info = json.loads(request.body)
-    new_sale = Sale.objects.create(**sale_info)
-    response_body = {
-        'Seller': new_sale.seller_id,
-        'comission': float(new_sale.comissions)
-    }
-        
-    return HttpResponse(
-                json.dumps(response_body),
-                content_type='application/json',
-                status=200)
+def create_sale(request): # Create Sale
+    if request.method == 'POST':  # Check request method
+        sale_info = json.loads(request.body)
+        form = SaleForm(sale_info)
+        if form.is_valid():   # Body request validation 
+            date = form.cleaned_data['date']    
+            amount = form.cleaned_data['amount']
+            seller = form.cleaned_data['seller']  
+
+            # Create a new sale
+            new_sale = Sale.objects.create(     
+                    date=date, amount=amount, seller=seller)
+
+            # Body content when form is valid.
+            response_body = dict(Seller=new_sale.seller_id, 
+                                 comission=float(new_sale.comissions))
+
+            # Since body content is valid, return response_body
+            return JsonResponse(response_body)
+
+        # Since body not valid, return errors       
+        else:
+            return JsonResponse(form.errors)   
+                                       
+    else:  # If method is different from POST, return body_content
+        body_content = {
+            'error': 'Method not allowed'
+        }
+        return JsonResponse(body_content)
 
 
 @csrf_exempt
