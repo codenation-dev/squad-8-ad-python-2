5e0586b0834761ff71848436ab79dd24aefe9fa9
messerzen <phzm.engmec@gmail.com>
2019-07-11 09:23:59 -0300

    add update_sale path in urls.py

--- sellgood/urls.py
+++ sellgood/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from views import sale
 
 urlpatterns = [
-    path('sale/create', sale.create_sale)
+    path('sale/create', sale.create_sale),
+    path('sale/update/<int:id_sale>')
 ]
