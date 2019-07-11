42023fbfdcfefe262ec1aefa5014752a78828724
messerzen <phzm.engmec@gmail.com>
2019-07-11 14:17:36 -0300

    Add list_sales_year url path in urls.py

--- sellgood/urls.py
+++ sellgood/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('sale/<int:id_sale>', sale.update_delete_sale),
     path('sale/seller/<int:id_seller>', views.sale_list_seller),
     re_path(r'sale/rank/(?P<month>\d{2}$)', views.list_sales_month,                       name='rank_sale_month'),
+    re_path(r'sale/rank/(?P<year>\d{4}$)', views.list_sales_year,                       name='rank_sale_year'),
 ]
