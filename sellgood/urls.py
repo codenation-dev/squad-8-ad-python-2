f10ab6540646713b5e10bd8c69740071df1f58f8
messerzen <phzm.engmec@gmail.com>
2019-07-11 12:25:43 -0300

    update url path 'sale/create' -> 'sale'

--- sellgood/urls.py
+++ sellgood/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from views import sale
 
 urlpatterns = [
-    path('sale/create', sale.create_sale),
+    path('sale', sale.create_read_sale),
     path('sale/update/<int:id_sale>')
 ]
