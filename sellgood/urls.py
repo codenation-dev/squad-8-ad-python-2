4443d66b6d1eaa7f51b04a246c442ebe8abe3217
messerzen <phzm.engmec@gmail.com>
2019-07-12 18:45:44 -0300

    Update urls.py: update name of each path

--- sellgood/urls.py
+++ sellgood/urls.py
@@ -3,15 +3,15 @@ from sellgood.views import sale
 
 urlpatterns = [
     # Sale paths - CRUD
-    path('sale', sale.create_read_sale),
-    path('sale/<int:id_sale>', sale.update_delete_sale),
+    path('sale', sale.create_read_sale, name='sale_create_read'),
+    path('sale/<int:id_sale>', sale.update_delete_sale,                             name='sale_update_delete'),
 
     # Sale list filter by seller
-    path('sale/seller/<int:id_seller>', sale.sale_list_seller),
+    path('sale/seller/<int:id_seller>', sale.sale_list_seller,                       name='sale_seller_read'),
 
     # Sale list filter by month - year - year/month
-    re_path(r'sale/rank/(?P<month>\d{2}$)', sale.list_sales_month,                       name='rank_sale_month'),
-    re_path(r'sale/rank/(?P<year>\d{4}$)', sale.list_sales_year,                       name='rank_sale_year'),
+    re_path(r'sale/rank/(?P<month>\d{2}$)', sale.list_sales_month,                       name='sale_rank_month'),
+    re_path(r'sale/rank/(?P<year>\d{4}$)', sale.list_sales_year,                       name='sale_rank_year'),
     re_path(r'sale/rank/(?P<year>\d{4})/(?P<month>\d{2}$)',   
-            sale.list_sales_year_month,                       name='rank_sale_year_month')
+            sale.list_sales_year_month,                       name='sale_rank_year_month')
 ]
