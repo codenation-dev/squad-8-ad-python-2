c5dc117bbdc3f8dd8a18b515d244ebf5e56bf9ed
flmoro <fernandoluizmoro@gmail.com>
2019-07-15 16:52:58 -0300

    Update urls names in test_commission module

--- sellgood/tests/test_commission.py
+++ sellgood/tests/test_commission.py
@@ -10,7 +10,7 @@ from sellgood.models import Sale, Seller, Plan
 
 def create_sellers():
     plan = Plan.objects.create(name='senior', 
-                                minimum_value=Decimal(50000.0),
+                                minimum_amount=Decimal(50000.0),
                                 lower_percentage=Decimal(0.02),
                                 higher_percentage=Decimal(0.1))   
 
@@ -35,69 +35,70 @@ def create_sellers():
 
 class RankViewTests(TestCase):
     def setUp(self):
-        self.cliente = Client()
+        self.client = Client()
 
         seller1, seller2 = create_sellers()
 
         Sale.objects.create(date='2018-12-01', 
-                            value=50000.00, 
+                            amount=50000.00, 
                             seller=seller1)        
 
         Sale.objects.create(date='2019-01-13', 
-                            value=1500.77, 
+                            amount=1500.77, 
                             seller=seller1)
 
         Sale.objects.create(date='2019-01-22', 
-                            value=44900.77, 
+                            amount=44900.77, 
                             seller=seller2)
 
     def test_no_records(self):
         Sale.objects.all().delete()
-        response = self.client.get(reverse('sellgood:rank'), args=['no'])
+        response = self.client.get(reverse('sellgood:commission_rank'), 
+                                           args=['no'])
         
         self.assertEqual(response.status_code, 404)              
         self.assertEqual(response.json()['error'], 'rank not found')
             
     def test_number_of_records(self):
-        response = self.client.get(reverse('sellgood:rank'))
+        response = self.client.get(reverse('sellgood:commission_rank'))
 
         self.assertEqual(response.status_code, 200)              
-        self.assertEqual(len(response.json()['rank']), 3)
+        self.assertEqual(len(response.json()['commission_rank']), 3)
 
     def test_commissions_order(self):        
-        response = self.client.get(reverse('sellgood:rank'))        
+        response = self.client.get(reverse('sellgood:commission_rank'))        
         
         self.assertEqual(response.status_code, 200)               
-        for idx, previous in enumerate(response.json()['rank']):            
-            for next in response.json()['rank'][idx+1:]:                
-                self.assertGreaterEqual(Decimal(previous['commission']), 
+        for idx, prev in enumerate(response.json()['commission_rank']):  
+            for next in response.json()['commission_rank'][idx+1:]:
+                self.assertGreaterEqual(Decimal(prev['commission']), 
                                         Decimal(next['commission']))
 
 
 class RankYearViewTests(TestCase):
     def setUp(self):
-        self.cliente = Client()
+        self.client = Client()
 
         seller1, seller2 = create_sellers()
 
         Sale.objects.create(date='2018-01-17', 
-                            value=55000.00, 
+                            amount=55000.00, 
                             seller=seller2) 
 
         Sale.objects.create(date='2018-12-01', 
-                            value=50000.00, 
+                            amount=50000.00, 
                             seller=seller1)        
 
         Sale.objects.create(date='2019-01-13', 
-                            value=1500.77, 
+                            amount=1500.77, 
                             seller=seller1)
 
         Sale.objects.create(date='2019-01-22', 
-                            value=44900.77, 
+                            amount=44900.77, 
                             seller=seller2)
 
     def test_no_records(self):        
-        response = self.client.get(reverse('sellgood:rank_year', 
+        response = self.client.get(reverse('sellgood:commission_rank_year', 
                                            kwargs={'year': 2017}))
         
         self.assertEqual(response.status_code, 404)              
@@ -105,47 +106,47 @@ class RankYearViewTests(TestCase):
                          'rank of year 2017 not found')
             
     def test_number_of_records(self):
-        response = self.client.get(reverse('sellgood:rank_year', 
+        response = self.client.get(reverse('sellgood:commission_rank_year', 
                                            kwargs={'year': 2019}))
 
         self.assertEqual(response.status_code, 200)              
-        self.assertEqual(len(response.json()['rank_year']), 2)
+        self.assertEqual(len(response.json()['commission_rank_year']), 2)
 
     def test_commissions_order(self):        
-        response = self.client.get(reverse('sellgood:rank_year', 
+        response = self.client.get(reverse('sellgood:commission_rank_year', 
                                            kwargs={'year': 2019}))     
         
         self.assertEqual(response.status_code, 200)               
-        for idx, previous in enumerate(response.json()['rank_year']):          
-            for next in response.json()['rank_year'][idx+1:]:                
-                self.assertGreaterEqual(Decimal(previous['commission']), 
+        for idx, prev in enumerate(response.json()['commission_rank_year']):
+            for next in response.json()['commission_rank_year'][idx+1:]:     
+                self.assertGreaterEqual(Decimal(prev['commission']), 
                                         Decimal(next['commission']))
 
 
 class RankMonthViewTests(TestCase):
     def setUp(self):
-        self.cliente = Client()
+        self.client = Client()
 
         seller1, seller2 = create_sellers()
 
         Sale.objects.create(date='2018-01-17', 
-                            value=55000.00, 
+                            amount=55000.00, 
                             seller=seller2) 
 
         Sale.objects.create(date='2019-12-01', 
-                            value=50000.00, 
+                            amount=50000.00, 
                             seller=seller1)        
 
         Sale.objects.create(date='2019-01-13', 
-                            value=1500.77, 
+                            amount=1500.77, 
                             seller=seller1)
 
         Sale.objects.create(date='2019-01-22', 
-                            value=44900.77, 
+                            amount=44900.77, 
                             seller=seller2)
 
     def test_no_records(self):        
-        response = self.client.get(reverse('sellgood:rank_month', 
+        response = self.client.get(reverse('sellgood:commission_rank_month', 
                                            kwargs={'month': 4}))
         
         self.assertEqual(response.status_code, 404)              
@@ -153,66 +154,70 @@ class RankMonthViewTests(TestCase):
                          'rank of month 4 not found')
             
     def test_number_of_records(self):
-        response = self.client.get(reverse('sellgood:rank_month', 
+        response = self.client.get(reverse('sellgood:commission_rank_month', 
                                            kwargs={'month': 1}))
 
         self.assertEqual(response.status_code, 200)              
-        self.assertEqual(len(response.json()['rank_month']), 3)
+        self.assertEqual(len(response.json()['commission_rank_month']), 3)
 
     def test_commissions_order(self):        
-        response = self.client.get(reverse('sellgood:rank_month', 
+        response = self.client.get(reverse('sellgood:commission_rank_month', 
                                            kwargs={'month': 1}))     
         
         self.assertEqual(response.status_code, 200)               
-        for idx, previous in enumerate(response.json()['rank_month']):         
-            for next in response.json()['rank_month'][idx+1:]:                
-                self.assertGreaterEqual(Decimal(previous['commission']), 
+        for idx, prev in enumerate(response.json()['commission_rank_month']):
+            for next in response.json()['commission_rank_month'][idx+1:]:
+                self.assertGreaterEqual(Decimal(prev['commission']), 
                                         Decimal(next['commission']))
 
                             
 class RankYearMonthViewTests(TestCase):
     def setUp(self):
-        self.cliente = Client()
+        self.client = Client()
 
         seller1, seller2 = create_sellers()
 
         Sale.objects.create(date='2018-01-17', 
-                            value=55000.00, 
+                            amount=55000.00, 
                             seller=seller2) 
 
         Sale.objects.create(date='2019-12-01', 
-                            value=50000.00, 
+                            amount=50000.00, 
                             seller=seller1)        
 
         Sale.objects.create(date='2019-01-13', 
-                            value=1500.77, 
+                            amount=1500.77, 
                             seller=seller1)
 
         Sale.objects.create(date='2019-01-22', 
-                            value=44900.77, 
+                            amount=44900.77, 
                             seller=seller2)
 
     def test_no_records(self):        
-        response = self.client.get(reverse('sellgood:rank_year_month', 
-                                           kwargs={'year': 2019, 'month': 4}))
+        response = self.client.get(reverse(
+            'sellgood:commission_rank_year_month', 
+            kwargs={'year': 2019, 'month': 4}))
         
         self.assertEqual(response.status_code, 404)              
         self.assertEqual(response.json()['error'], 
                          'rank of year 2019 and month 4 not found')
             
     def test_number_of_records(self):
-        response = self.client.get(reverse('sellgood:rank_year_month', 
-                                           kwargs={'year': 2019, 'month': 1}))
+        response = self.client.get(reverse(
+            'sellgood:commission_rank_year_month', 
+            kwargs={'year': 2019, 'month': 1}))
 
         self.assertEqual(response.status_code, 200)              
-        self.assertEqual(len(response.json()['rank_year_month']), 2)
+        self.assertEqual(len(response.json()['commission_rank_year_month']), 2)
 
     def test_commissions_order(self):        
-        response = self.client.get(reverse('sellgood:rank_year_month', 
-                                           kwargs={'year': 2019, 'month': 1})) 
+        response = self.client.get(reverse(
+            'sellgood:commission_rank_year_month', 
+            kwargs={'year': 2019, 'month': 1})) 
         
-        self.assertEqual(response.status_code, 200)               
-        for idx, previous in enumerate(response.json()['rank_year_month']):    
-            for next in response.json()['rank_year_month'][idx+1:]:            
-                self.assertGreaterEqual(Decimal(previous['commission']), 
+        self.assertEqual(response.status_code, 200) 
+        response = response.json()['commission_rank_year_month']
+        for idx, prev in enumerate(response):    
+            for next in response[idx+1:]:            
+                self.assertGreaterEqual(Decimal(prev['commission']), 
                                         Decimal(next['commission']))
