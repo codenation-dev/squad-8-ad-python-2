cc075b7f8552338909a023e92e746f45b87893ce
messerzen <phzm.engmec@gmail.com>
2019-07-18 20:26:33 -0300

    Update sale.py: delete ListAPIView (year month)

--- sellgood/views/sale.py
+++ sellgood/views/sale.py
@@ -7,17 +7,3 @@ class SaleViewSet(viewsets.ModelViewSet):
     queryset = Sale.objects.all()
     serializer_class = SaleSerializer  
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-
-class ListSaleYearMonth(generics.ListAPIView):
-    serializer_class = SaleSerializer
-
-    def get_queryset(self):
-        month = self.kwargs.get('month')
-        year = self.kwargs.get('year')
-        if month and not year:
-            return Sale.objects.filter(date__month=month)
-        elif year and not month:
-            return Sale.objects.filter(date__year=year)
-        elif year and month:
-            return Sale.objects.filter(date__year=year, date__month=month)
