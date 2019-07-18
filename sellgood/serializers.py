3a704271d7a2c09df9ced813fa1aaa8debcac66e
flmoro <fernandoluizmoro@gmail.com>
2019-07-17 22:16:38 -0300

    Add AddressSerializer class

--- sellgood/serializers.py
+++ sellgood/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 
-from sellgood.models import Sale, Seller
+from sellgood.models import Address, Sale, Seller
+
+class AddressSerializer(serializers.ModelSerializer):       
+    class Meta:
+        model = Address
+        fields = '__all__'
 
 
 class CommissionSerializer(serializers.ModelSerializer):
-    #seller = serializers.PrimaryKeyRelatedField(queryset=Seller.objects.all())
-        
     class Meta:
         model = Sale
         fields = ['date', 'commission', 'seller']
