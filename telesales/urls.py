80e84a3bc734aad6b575ea8074ccc7dfdc3201d4
flmoro <fernandoluizmoro@gmail.com>
2019-07-17 21:59:55 -0300

    Add API authentication url

--- telesales/urls.py
+++ telesales/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sellgood/', include('sellgood.urls'))
+    path('sellgood/', include('sellgood.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
