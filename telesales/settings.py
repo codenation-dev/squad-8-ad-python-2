b436cf44fab3f3eb80430b0c5aab1923893eeddf
flmoro <fernandoluizmoro@gmail.com>
2019-07-18 16:44:12 -0300

    Add pagination to the API web page

--- telesales/settings.py
+++ telesales/settings.py
@@ -96,6 +96,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {    
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
+    'DEFAULT_PAGINATION_CLASS':                                                    'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
