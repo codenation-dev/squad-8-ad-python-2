5b5c1a46fc99cbca1cb42e1d8bfecc7992030fb8
messerzen <phzm.engmec@gmail.com>
2019-07-19 18:00:31 -0300

    Update Serializers import in views

--- sellgood/views/address.py
+++ sellgood/views/address.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, permissions
 
 from sellgood.models import Address
-from sellgood.serializers import AddressSerializer
+from sellgood.serializers.serializers_address import AddressSerializer
 
 
 class AddressViewSet(viewsets.ModelViewSet):
