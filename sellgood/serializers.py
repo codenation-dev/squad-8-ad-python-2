4d8198bebc4521154b5cc7bd180049863fc74bd9
messerzen <phzm.engmec@gmail.com>
2019-07-19 17:46:52 -0300

    Create serializers_address

--- sellgood/serializers.py
+++ sellgood/serializers.py
@@ -3,12 +3,6 @@ from rest_framework import serializers
 from sellgood.models import Address, Sale, Seller
 
 
-class AddressSerializer(serializers.ModelSerializer):       
-    class Meta:
-        model = Address
-        fields = '__all__'
-        
-
 class CommissionSerializer(serializers.ModelSerializer):
     seller_id = serializers.IntegerField(source='seller.id')
     seller_name = serializers.CharField(source='seller.name')
