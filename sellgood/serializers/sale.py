1bc8c9e3687ed9d34b5a4c6e262353369a695b37
flmoro <fernandoluizmoro@gmail.com>
2019-07-24 14:57:29 -0300

    Add to_internal_value in SaleSerializer class
    This method changes the day to the last day of the month before
    validation occurs. It was not possible to implement this
    procedure in the save method of the Sale model.

--- sellgood/serializers/sale.py
+++ sellgood/serializers/sale.py
@@ -1,3 +1,6 @@
+import calendar
+from datetime import datetime
+
 from rest_framework import serializers
 
 from sellgood.models import Sale
@@ -6,3 +9,11 @@ class SaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sale
         fields = '__all__'
+
+    def to_internal_value(self, data):
+        data = data.copy()
+        data['date'] = datetime.strptime(data['date'], '%Y-%m-%d')
+        day = calendar.monthrange(data['date'].year, data['date'].month)[1]
+        data['date'] = data['date'].replace(day=day).strftime('%Y-%m-%d')
+        
+        return super().to_internal_value(data)
