184caa644b3da6c4c42ea5ff38163f36ac4a8bc1
messerzen <phzm.engmec@gmail.com>
2019-07-27 14:50:06 -0300

    Update seller: refactor @action sale

--- sellgood/views/seller.py
+++ sellgood/views/seller.py
@@ -23,6 +23,10 @@ class SellerViewSet(viewsets.ModelViewSet):
     
     @action(detail=True)
     def sale(self, request, pk=None):
-        serializer = SaleSerializer(Sale.objects.filter(seller_id=pk), 
-        return Response(serializer.data)
+        query = Sale.objects.filter(seller_id=pk)
+        serializer = SaleSerializer(query, many=True)
+        if query:
+            return Response(serializer.data)
+        else:
+            return Response({'detail': 'Not found.'}, status=status.HTTP_200_OK)
         
